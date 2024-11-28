7b5ac09e55562fb8ee98791526d930c53f38eb1d
Zyad Dev <zyadt870@gmail.com>
2024-11-29 01:11:27 +0300

    Work preparation

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Footer from '../components/footer';
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Changa', 'sans-serif'], // سيحل مكان الخط الافتراضي
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#232f3e',
+          secondary: '#007955',
+          accent: '#131921',
+          neutral: '#002926',
+          'base-100': '#e3e6e6',
+          info: '#008ace',
+          success: '#009b6d',
+          warning: '#d96d00',
+          error: '#ff3c4f',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+};
+
